b4a9b704a76fe2d180a5a3136082fbdcc715416c
Aldernei <josejunior3979@gmail.com>
2022-12-05 08:38:13 -0300

    modificando formatado das imagens

--- scriptteste.py
+++ scriptteste.py
@@ -2,9 +2,10 @@ import pytesseract
 import cv2
 
 #Lendo a imagem ou grupo de imagens
+imagem = cv2.imread("C:\workspace\pythonOCR\images\exemplo2.png")
 caminho = r"C:\Program Files\Tesseract-OCR"
 pytesseract.pytesseract.tesseract_cmd = caminho + r"\tesseract.exe"
-imagem = cv2.imread(r"C:\workspace\pythonworkana\images\exemplo4.jpg")
+
 
 
 #Extraindo o texto
