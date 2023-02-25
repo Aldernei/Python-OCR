5fe7429f3873a447afba6e7e1e882c0c986c5a9c
Aldernei <josejunior3979@gmail.com>
2023-02-25 20:05:01 -0300

    melhorias e tratamentos de erros

--- scriptteste.py
+++ scriptteste.py
@@ -1,14 +1,20 @@
 import pytesseract
 import cv2
 
-#Lendo a imagem ou grupo de imagens
-imagem = cv2.imread("C:\workspace\pythonOCR\images\exemplo2.png")
-caminho = r"C:\Program Files\Tesseract-OCR"
-pytesseract.pytesseract.tesseract_cmd = caminho + r"\tesseract.exe"
+# Lendo a imagem
+imagem = cv2.imread("images/exemplo2.png")
 
+# Verificando se a imagem foi lida corretamente
+if imagem is None:
+    print("Erro ao ler a imagem")
+else:
+    # Configurando o caminho do executável do Tesseract
+    caminho_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    pytesseract.pytesseract.tesseract_cmd = caminho_tesseract
 
-
-#Extraindo o texto
-
-texto = pytesseract.image_to_string(imagem)
-print(texto)
+    # Extraindo o texto da imagem
+    try:
+        texto = pytesseract.image_to_string(imagem)
+        print(texto)
+    except Exception as e:
+        print("Erro ao extrair texto da imagem:", e)
